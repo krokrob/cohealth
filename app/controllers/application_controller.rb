712660ca3183cd8b4c667181492362903ec2393d
kevin robert <kevin.j.robert@gmail.com>
2019-10-21 00:13:57 +0200

    force pundit everywhere

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-  after_action :verify_authorized, except: :index unless devise_controller?
-  after_action :verify_policy_scoped, only: :index unless devise_controller?
+  after_action :verify_authorized, except: :index, unless: :devise_controller?
+  after_action :verify_policy_scoped, only: :index, unless: :devise_controller?
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
