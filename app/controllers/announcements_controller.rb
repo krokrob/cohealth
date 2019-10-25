93696ecdff9b2c0530fd261ca77edb1d314aade2
kevin robert <kevin.j.robert@gmail.com>
2019-10-25 08:20:34 +0200

    change form design

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -1,13 +1,17 @@
 class AnnouncementsController < ApplicationController
   skip_after_action :verify_policy_scoped, only: :index
+
   def index
-    if params[:keyword]
-      @keyword = params[:keyword]
-      @headers = ['Catégorie', 'Bodacc', 'Annonce n°', 'Siren', 'RCS', 'Nom', 'Prénom',
-        'Dénomination', 'Forme juridique', 'Capital', 'Administration',
-        'Adresse', 'Qualité', 'Origine du fond', 'Activité', 'Adresse établissement',
-        'Début Activité', 'Date de création', 'URL infogreffe', 'NAF']
-      @announcements = BodaccScraper.new.announcements(@keyword)
-    end
+    return unless params[:keyword]
+
+    @keyword = params[:keyword]
+    @headers = [
+      'Catégorie', 'Bodacc', 'Annonce n°', 'Siren', 'RCS', 'Nom', 'Prénom',
+      'Dénomination', 'Forme juridique', 'Capital', 'Administration',
+      'Adresse', 'Qualité', 'Origine du fond', 'Activité', 'Adresse établissement',
+      'Adresse du siège social', 'Début Activité', 'Date de création',
+      'URL infogreffe', 'NAF', 'Siren précédent', 'Admin précédent'
+    ]
+    @announcements = BodaccScraper.new.announcements(@keyword)
   end
 end
